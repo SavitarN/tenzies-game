6982f12bf5e3cb165fe517263495f2ce2c2fc4fe
SavitarN <nischalzoc999@gmail.com>
2025-01-22 21:25:14 +0545

    functionality

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./style.css";
 const Buttons = (props) => {
+  const style = {
+    backgroundColor: props.isHeld ? "lightgreen" : "white",
+  };
   return (
     <div>
-      <button>{props.value}</button>
+      <button style={style} onClick={(e) => props.onHold(props.id)}>
+        {props.value}
+      </button>
     </div>
   );
 };
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,66 @@
 import React from "react";
 import "./style.css";
 import Buttons from "./Buttons";
-const Main = () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+import { nanoid } from "nanoid";
 
-  const btnElem = arr.map((elem) => <Buttons value={elem} />);
+const Main = () => {
+  const [button, setButton] = React.useState(() => buttonElements());
+  const [status, setStatus] = React.useState(false);
+
+  //funciton for generating object that holds the dice element essential property//
+  function buttonElements() {
+    const arr = [];
+    for (let i = 0; i <= 9; i++) {
+      const randomNumber = {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      };
+
+      arr.push(randomNumber);
+    }
+    return arr;
+  }
+
+  //function to hold the dice
+  function hold(id) {
+    setButton((prevArr) =>
+      prevArr.map((elem) =>
+        elem.id === id ? { ...prevArr, isHeld: !elem.isHeld } : elem
+      )
+    );
+  }
+
+  //to check game won condition based on state value//
+
+  const gameWon =
+    button.every((elem) => elem.isHeld) &&
+    button.every((elem) => die.value === button[0].value);
+  //function for roll dice when we hold our numbers//
+
+  function handleRoll() {
+    setButton((buttons) =>
+      buttons.map((elem) =>
+        elem.isHeld ? elem : { ...elem, value: Math.ceil(Math.random() * 6) }
+      )
+    );
+  }
+  const btnElem = button.map((elem) => (
+    <Buttons
+      value={elem.value}
+      isHeld={elem.isHeld}
+      key={elem.id}
+      id={elem.id}
+      onHold={hold}
+    />
+  ));
   return (
     <div className="container">
       <div className="dieContainer">{btnElem}</div>
 
-      <button className="btnRoll">Roll Dice</button>
+      <button className="btnRoll" onClick={handleRoll}>
+        {gameWon ? "New Game" : "Roll Dice"}
+      </button>
     </div>
   );
 };
