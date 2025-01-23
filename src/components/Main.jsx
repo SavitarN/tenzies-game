786cb78858cc246b084c49552004fa0d9c755fa9
SavitarN <nischalzoc999@gmail.com>
2025-01-23 21:57:46 +0545

    styling and text part

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -57,6 +57,11 @@ const Main = () => {
   ));
   return (
     <div className="container">
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       {gameWon && <Confetti />}
       <div className="dieContainer">{btnElem}</div>
 
